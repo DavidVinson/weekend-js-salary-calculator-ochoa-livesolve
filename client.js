cd8cb641abb56eb0751382d1761b6e529b250990
David Vinson <davidvinson018@gmail.com>
2025-01-13 09:37:32 -0600

    form handler setup

--- client.js
+++ client.js
@@ -1,1 +1,11 @@
-console.log('client is working...');
+function onReady() {
+  console.log('client is working...');
+}
+
+function formHandler(event) {
+  event.preventDefault();
+  console.log('handle the form');
+}
+
+//call onReady when DOM is ready
+onReady();
