a33eb1c1a3cc591e99850097d537d695f17da62c
David Vinson <davidvinson018@gmail.com>
2025-01-13 14:55:56 -0600

    refactor to only work with the dom

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-let total = 0;
+let totalMonthlyCost = 0;
 
 function onReady() {
   console.log('client is working...');
@@ -14,43 +14,34 @@ function formHandler(event) {
   const employeeIdEl = document.getElementById('id-input');
   const employeeTitleEl = document.getElementById('job-title-input');
   const employeeAnnualSalaryEl = document.getElementById('annual-salary-input');
-  //   console.log(
-  //     'employee',
-  //     employeeFirstNameEl,
-  //     employeeLastNameEl,
-  //     employeeIdEl,
-  //     employeeTitleEl,
-  //     employeeAnnualSalaryEl
-  //   );
 
-  //   const newEmployee = {
-  //     firstName: employeeFirstNameEl.value,
-  //     lastName: employeeLastNameEl.value,
-  //     id: employeeIdEl.value,
-  //     title: employeeTitleEl.value,
-  //     salary: parseInt(employeeAnnualSalaryEl.value),
-  //   };
+  //create newEmployee object
+  const newEmployee = {
+    firstName: employeeFirstNameEl.value,
+    lastName: employeeLastNameEl.value,
+    id: employeeIdEl.value,
+    title: employeeTitleEl.value,
+    salary: parseInt(employeeAnnualSalaryEl.value),
+  };
 
-  //   console.log('newEmployee', newEmployee);
+  console.log('newEmployee', newEmployee);
 
-  //   <tr>
-  //             <td>Joe</td>
-  //             <td>Smith</td>
-  //             <td>4</td>
-  //             <td>Dev</td>
-  //             <td>4500</td>
-  //           </tr>
+  displayEmployees(newEmployee);
 
-  const tableBody = document.getElementById('employee-list');
-  tableBody.innerHTML += `
-  <tr>
-    <td>${employeeFirstNameEl.value}</td>
-    <td>${employeeLastNameEl.value}</td>
-    <td>${employeeIdEl.value}</td>
-    <td>${employeeTitleEl.value}</td>
-    <td>${parseInt(employeeAnnualSalaryEl.value)}</td>
-    <td><button onclick="removeEmployee(event)">remove emp</button></td>
-  </tr>`;
+  //get total monthly element
+  const totalMonthlyEl = document.getElementById('total-monthly');
+  //   console.log('total', totalMonthlyEl);
+
+  totalMonthlyCost += parseInt(employeeAnnualSalaryEl.value) / 12;
+  console.log('totalMonthlyCost', totalMonthlyCost);
+
+  if (totalMonthlyCost > 20000) {
+    //add a class to turn the span red
+    // totalMonthlyEl.style.background = 'red';
+    totalMonthlyEl.classList.add('red-background');
+  }
+
+  totalMonthlyEl.textContent = `${totalMonthlyCost.toFixed(2)}`;
 
   //clear form fields
   //   employeeFirstNameEl.value = '';
@@ -59,28 +50,37 @@ function formHandler(event) {
   //   employeeTitleEl.value = '';
   //   employeeAnnualSalaryEl.value = '';
 
-  //get total monthly element
-  const totalMonthlyEl = document.getElementById('total-monthly');
-  //   console.log('total', totalMonthlyEl);
-
-  total += parseInt(employeeAnnualSalaryEl.value) / 12;
-  console.log('total', total);
-
-  if (total > 20000) {
-    //add a class to turn the span red
-    // totalMonthlyEl.style.background = 'red';
-    totalMonthlyEl.classList.add('red');
-  }
-
-  totalMonthlyEl.textContent = `${total.toFixed(2)}`;
-
   //clears form inputs using form method .reset()
-  //   document.querySelector('form').reset();
+  document.querySelector('form').reset();
 }
 
-function removeEmployee(event, annualSalary) {
-  console.log('remove this employee and subtract this amount');
-  event.target.closest('tr').remove();
+function displayEmployees(employee) {
+  const tableBody = document.getElementById('employee-list');
+
+  tableBody.innerHTML += `
+    <tr id=${employee.id}>
+      <td>${employee.firstName}</td>
+      <td>${employee.lastName}</td>
+      <td>${employee.id}</td>
+      <td>${employee.title}</td>
+      <td>${parseInt(employee.salary)}</td>
+      <td><button onclick="removeEmployee(event, ${parseInt(employee.salary)})">remove emp</button></td>
+    </tr>`;
+}
+
+function removeEmployee(event, salary) {
+  // Get the employee row to be deleted
+  const employeeRow = event.target.closest('tr');
+
+  //get the total monthly element
+  const totalMonthlyEl = document.getElementById('total-monthly');
+
+  totalMonthlyCost -= salary / 12;
+  //update dom with new monthly salary
+  totalMonthlyEl.textContent = `${totalMonthlyCost.toFixed(2)}`;
+
+  // Remove the employee row from the table
+  employeeRow.remove();
 }
 
 //call onReady when DOM is ready
